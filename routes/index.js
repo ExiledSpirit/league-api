318286d5db0dd8757ce3b35a27fc0d319555e028
Gabriel Gonçalvas <gabriel.goncalves@drz.local>
2022-06-27 09:31:36 -0300

    Removendo console logs e refatorando dotenv

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {getPUUID, getMatches, byMatchId} = require('../riot-api/riot-api');
+const { getPUUID, getMatches, byMatchId } = require('../riot-api/riot-api');
 
 router.get('/partidas/:username', async (req, res) => {
   const PUUID = await getPUUID(req.params.username);
-  console.log(`PUUID = ${PUUID}`);
   const matchIds = await getMatches(PUUID);
-  console.log(`matchIds = ${matchIds}`);
   matches = await Promise.all(matchIds?.map((matchId) => byMatchId(matchId)));
-  console.log(`matches = ${matches}`);
+
   res.json(matches);
 });
 
